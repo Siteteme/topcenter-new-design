4f346edd9c64d6428ccd28af243f10b5e2c00268
farid <siteteme@gmail.com>
2022-05-23 16:22:47 +0430

    optimize carusel and edited

---
+++ js/main.js
@@ -0,0 +1,45 @@
+// carusel brands
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+
+    loop: true,
+    slidesPerView: 4,
+    spaceBetween: 60,
+    autoplay: true,
+
+    // If we need pagination
+    pagination: {
+    
+        clickable: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    
+
+
+});
+
+
+
+
+// transition
+var wow = new wow({
+    boxClass: 'wow', // animated element css class (default is wow)
+    animateClass: 'animated', // animation css class (default is animated)
+    offset: 0, // distance to the element when triggering the animation (default is 0)
+    mobile: true, // trigger animations on mobile devices (default is true)
+    live: true, // act on asynchronously loaded content (default is true)
+    callback: function (box) {
+        // the callback is fired every time an animation is started
+        // the argument that is passed in is the DOM node being animated
+    },
+    scrollContainer: null, // optional scroll container selector, otherwise use window,
+    resetAnimation: true, // reset animation on end (default is true)
+});
+wow.init();
